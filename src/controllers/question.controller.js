8630aca442740f7a625f1f11f79fbb7b2f231cfa
Viktor Bulvarenko <silken.ua@gmail.com>
2019-02-15 23:52:11 +0200

    fix schema question, answers. Create route question for test response query to font-end

---
+++ src/controllers/question.controller.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+const Questions = require('../models/questions.model');
+
+module.exports.getQuestionByNumber = async ( req, res ) => {
+    const dataSend = {};
+    const getQuestionByNumber = await Questions.findOne({
+      questionNumber: req.params.number
+    }).lean();
+
+    dataSend.question = getQuestionByNumber;
+
+    if (getQuestionByNumber) {
+      res.status(200).json(dataSend);
+    }
+};
+
+module.exports.getFirstQuestion = async ( req, res ) => {
+  const dataSend = {};
+
+  const questionCount = await Questions.find().exec(function (err, results) {
+    if (err) console.log(err);
+
+    const count = results.length;
+    dataSend.countQuestion = count;
+  });
+
+  const getFirst = await Questions.findOne({ questionNumber: 1 }).lean().exec();
+
+  dataSend.question = getFirst;
+  dataSend.userAnswerId = 'kjhlasfdkljhasdflkjhaf';
+
+  if (getFirst) {
+    return res.status(200).json(dataSend);
+  }
+};
+
+
